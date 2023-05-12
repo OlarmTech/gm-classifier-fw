1b7bcc04181e102c294cee24eb9850379cc742f4
Alvin Reabow <alvin@olarm.com>
2023-05-12 15:29:12 +0200

    convert main to cpp

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/tflite_bridge/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "model.h"
+
+extern "C" void app_main(void)
+{
+    while(1) {
+
+    }
+}
