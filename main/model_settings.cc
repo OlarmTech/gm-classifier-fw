29017e48e5fccdaf2574f771161ab656c14a20de
Alvin Reabow <alvin@olarm.com>
2023-05-19 14:20:31 +0200

    create model settings config

---
+++ main/model_settings.cc
@@ -0,0 +1,12 @@
+#include "model_settings.h"
+
+const char* kCategoryLabels[kCategoryCount] = {
+    "acfail",
+    "batterydc",
+    "closed",
+    "closing",
+    "collisions",
+    // "courtesylight",
+    "open",
+    "opening"
+};
