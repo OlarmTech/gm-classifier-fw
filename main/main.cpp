7c95cdf40bb71584008ab13f98c1e097471190e0
Alvin Reabow <alvin@olarm.com>
2023-05-23 13:51:26 +0200

    LNK-1115 add etdrive500 prototype

--- main/main.cpp
+++ main/main.cpp
@@ -4,11 +4,13 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "model.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
+#include "model_settings.h"
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
@@ -48,7 +50,6 @@ tflite::ErrorReporter *error_reporter = nullptr;
 // Create an area of memory to use for input, output, and intermediate arrays.
 // The size of this will depend on the model you're using, and may need to be
 // determined by experimentation.
-
 constexpr int kTensorArenaSize = 2 * 1024;
 uint8_t tensor_arena[kTensorArenaSize];
 float* model_input_buffer = nullptr;
@@ -57,15 +58,47 @@ float* model_input_buffer = nullptr;
 extern "C" void app_main(void)
 {
     //-------------TF Config---------------//
-
+    model = tflite::GetModel(nn_model);
+    if (model->version() != TFLITE_SCHEMA_VERSION) {
+        TF_LITE_REPORT_ERROR(error_reporter, "Model provided is schema version %d not equal to supported "
+                "version %d.", model->version(), TFLITE_SCHEMA_VERSION);
+        return;
+    }
     // pull in all required operations
+    static tflite::MicroMutableOpResolver<4> micro_op_resolver;
+    if (micro_op_resolver.AddFullyConnected() != kTfLiteOk) {
+        return;
+    }
+    if (micro_op_resolver.AddMul() != kTfLiteOk) {
+        return;
+    }
+    if (micro_op_resolver.AddSub() != kTfLiteOk) {
+        return;
+    }
+    if (micro_op_resolver.AddLogistic() != kTfLiteOk) {
+        return;
+    }
 
     // Build an interpreter to run the model with.
-
+    static tflite::MicroInterpreter static_interpreter(model, micro_op_resolver, tensor_arena, kTensorArenaSize);
+    interpreter = &static_interpreter;
     // Allocate memory from the tensor_arena for the model's tensors.
-
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk) {
+        TF_LITE_REPORT_ERROR(error_reporter,"AllocateTensors() failed");
+    return;
+    }
     // Get information about the memory area to use for the model's input.
-
+    model_input = interpreter->input(0);
+    if (
+    (model_input->dims->size != 2) || 
+    (model_input->dims->data[0] != 1) || 
+    (model_input->dims->data[1] != kFeatureElementCount) ||
+    (model_input->type != kTfLiteFloat32)) {
+        TF_LITE_REPORT_ERROR(error_reporter,"Bad input tensor parameters in model");
+    return;
+    }
+    model_input_buffer = model_input->data.f;
     //-------------End TF Config---------------//
 
     //-------------ADC1 Init---------------//
@@ -89,45 +122,98 @@ extern "C" void app_main(void)
     ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw[0][0], &voltage[0][0]));
     
     // Start sys timer count
+    static uint64_t current_time = esp_timer_get_time() / 1000;
     
     while (1) {
         int adc_avg = 0;
         // read ADC and average over 10 samples
         for (int i = 0; i<10; i++){
-
+            ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_6, &adc_raw[0][0]));
+            adc_avg += adc_raw[0][0];
         }
         adc_avg /= 10;
 
         if (do_calibration) {
-
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_avg, &voltage[0][0]));
         }
+
+        current_state = get_current_state(voltage[0][0], 1500);
         
         // timeout
         if (esp_timer_get_time() / 1000 - current_time >= kStateTimeout){
-
+            state_buffer[state_counts] = 2500.0/1000.0;
+            // feature_buffer[9-state_counts] = 2500.0/1000.0;
+            current_time = esp_timer_get_time() / 1000;
+            // feature_buffer[10] = (float) current_state;
+            running_mean += state_buffer[state_counts]*(float)current_state;
+            state_counts +=1;
+            timeout = true;
+            // shift state buffer to be zero filled from the beginning
+            shiftArray(state_buffer, kStateCount, kStateCount - state_counts);
         }
         // state change
         else if (current_state != previous_state) {
-
+            state_buffer[state_counts] = (esp_timer_get_time() / 1000 - current_time) / 1000.0;
+            current_time = esp_timer_get_time() / 1000;
+            running_mean += state_buffer[state_counts]*(float)previous_state;
+            ESP_LOGI(TAG, "State: %d, Time: %f", previous_state, state_buffer[state_counts]);
+            state_counts +=1;
         }
         else {
         }
 
         if (state_counts >= kStateCount || timeout){
+            float sum = 0;
+            for(int i = 0; i < kStateCount; i++){
+                sum += state_buffer[i];
+            }
+            float mean = running_mean/sum;
  
             //-------------Run Inference---------------//
             // Fill input buffer of tfLite model
-
+            for (int i = 0; i < kStateCount; i++) {
+                model_input_buffer[i] = state_buffer[i];
+            }
+            model_input_buffer[11] = mean;  // set mean
+            model_input_buffer[10] = (float) previous_state; //
             //-------------Print Input Buffer---------------//
-
+            ESP_LOGI(TAG, "=======Input Buffer=======");
+            for (int i = 0; i < kFeatureElementCount; i++){
+                ESP_LOGI(TAG, "Input Data: %f", model_input_buffer[i]);
+            }
             // Run the model on the input and make sure it succeeds.
+            TfLiteStatus invoke_status = interpreter->Invoke();
+            if (invoke_status != kTfLiteOk) {
+                TF_LITE_REPORT_ERROR(error_reporter,"Invoke failed");
+                return;
+            }
 
+            // Obtain a pointer to the output tensor
+            TfLiteTensor* output = interpreter->output(0);
+            if (
+                (output->dims->size != 2) ||
+                (output->dims->data[0] != 1) ||
+                (output->dims->data[1] != kCategoryCount)
+                ) 
+                {
+                    TF_LITE_REPORT_ERROR(error_reporter, "The results for recognition should contain %d elements, but there are %d in an %d-dimensional shape",kCategoryCount, output->dims->data[1],output->dims->size);
+                    return;
+                }
+            if (output->type != kTfLiteFloat32) {
+                TF_LITE_REPORT_ERROR(error_reporter, "The results for recognition should be Float32 elements, but are %d",output->type);
+                return;
+            }
+            float *result_buffer = tflite::GetTensorData<float>(output);
+            ESP_LOGI(TAG, "=====Output Prediction=====");
+            for (int i = 0; i < kCategoryCount; i++) {
+                ESP_LOGI(TAG, "%s: %.3f", kCategoryLabels[i],result_buffer[i]);
+            }
 
-            // Obtain a pointer to the output tensor and read the predicted label
-
-            //-------------Print Output Buffer---------------//
- 
-            // reset values
+            for (size_t i = 0; i < kFeatureElementCount; ++i) {model_input_buffer[i] = 0;}
+            for (size_t i = 0; i < kStateCount; ++i) {state_buffer[i] = 0.0;}
+            running_mean = 0;
+            timeout = false;
+            state_counts = 0;
         }
         previous_state = current_state;
         // keep the doggo happy
@@ -139,6 +225,38 @@ extern "C" void app_main(void)
         adc_calibration_deinit(adc1_cali_handle);
     }
 
+}
+
+void shiftArray(float arr[], int size, int n) {
+    int i, j;
+    float temp;
+
+    // Perform n right shifts
+    for (i = 0; i < n; i++) {
+        // Store the last element
+        temp = arr[size - 1];
+
+        // Shift all elements one position to the right
+        for (j = size - 1; j > 0; j--) {
+            arr[j] = arr[j - 1];
+        }
+
+        // Place the last element at the beginning
+        arr[0] = temp;
+    }
+}
+
+void print_output(float* result) {
+    ESP_LOGI(TAG, "======Output:======");
+    for (int i = 0; i < kCategoryCount; i++)
+    {
+        ESP_LOGI(TAG,"%s: %.4f", kCategoryLabels[i],result[i]);
+    }
+    ESP_LOGI(TAG, "===================");
+}
+
+uint get_current_state(int voltage, int threshold){
+    return voltage > threshold ? 1 : 0;
 }
 
 
---
+++ main/model_settings.h
@@ -0,0 +1,5 @@
+constexpr int kFeatureElementCount = 12;
+constexpr int kCategoryCount = 7;
+constexpr int kStateCount = 10;
+constexpr int kStateTimeout = 2500;
+extern const char* kCategoryLabels[kCategoryCount];
