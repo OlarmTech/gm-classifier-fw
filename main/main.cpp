d9d90619588ca4944875b673750c988fb74fde9a
Alvin Reabow <alvin@olarm.com>
2023-05-23 13:25:14 +0200

    LNK-1115 test PR

--- main/main.cpp
+++ main/main.cpp
@@ -50,7 +50,6 @@ tflite::ErrorReporter *error_reporter = nullptr;
 // Create an area of memory to use for input, output, and intermediate arrays.
 // The size of this will depend on the model you're using, and may need to be
 // determined by experimentation.
-
 constexpr int kTensorArenaSize = 2 * 1024;
 uint8_t tensor_arena[kTensorArenaSize];
 float* model_input_buffer = nullptr;
